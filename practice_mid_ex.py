77a3244504fe8254b606ed0fdc11c37abd324fd4
JunHakLee03 <junhak111701@gmail.com>
2022-04-15 20:49:01 +0900

    20220415 upload

---
+++ practice_mid_ex.py
@@ -0,0 +1,73 @@
+#s1 = '%6d %-6d' % (10, 20)
+#s2 = '%7.2f : %07.2f' % (3.14, 4.14)
+#s3 = '%6x : %6x' % (0xf, 15)
+#print(s1)
+#print(s2)
+#print(s3)
+
+#s='0b01001'
+#print(int(s, 2))
+
+#n = True
+#m = 'True'
+#print(n, type(n))
+#print(m, type(m))
+
+#a = 2 > 3
+#b = 2 < 4
+#print(a, b)
+
+#n = '홍길동\n\\ \' \" \n '
+#print(n)
+
+#n = -1
+#m = 1
+#n = n >> 1
+#m = m >> 1
+#print(n, bin(n))
+#print(m, bin(m))
+#n, m = n >> 1, m >> 1
+#print(n, bin(n))
+#print(m, bin(m))
+#n , m = n << 2 , m << 2
+#print(n, bin(n))
+#print(m, bin(m))
+
+#total = 0
+#for i in range(0, 100) :
+#    if i % 5 != 0:
+#        continue
+#    total += i
+
+#print('Total= ', total)
+
+
+#n = 10
+#m = (4 + 6)
+#p = (10)
+#q = (4+6,)
+#r = (10,)
+#print(type(n))
+#print(type(m))
+#print(type(p))
+#print(type(q))
+#print(type(r))
+
+#def area(w, h) :
+#    return w * h
+
+#print( area(2, 3) )
+
+#version = 1.0
+
+#def newVersion(v) :
+#    version = v
+#    print( 'My version = %.1f' % version )
+
+#def printVersion() :
+#    print('Hello.Current ver. : %.1f' % version )
+
+#printVersion()
+#newVersion( 1.5 )
+#printVersion()
+
