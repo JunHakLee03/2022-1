7968b2d1787f043b7a57cb96e9a0b36e784b577b
JunHakLee03 <junhak111701@gmail.com>
2022-04-16 23:36:47 +0900

    20220416 upload

--- practice_mid_ex.py
+++ practice_mid_ex.py
@@ -71,3 +71,74 @@
 #newVersion( 1.5 )
 #printVersion()
 
+
+#number = 0
+#check = 1
+#for a in range (0, 4):
+#    for i in range (1, 16) :
+#        if i & check != 0 :
+#            print(i, end=" ")
+#    print('생각한 숫자가 있나')
+#    answer = input('Y/N: ')
+#    while True :
+#        if answer == 'Y' :
+#            number = number | check
+#            break
+#        elif answer == 'N' :
+#            break
+#        else:
+#            answer = input('Y/N으로 대답하시오: ')
+#    check = check * 2
+#print('예측: ', number)
+
+#import turtle
+#import random
+#import math
+#t = turtle.Turtle()
+
+#def drawsquare(x):
+#    for _ in range(0, 4):
+#        t.forward(x)
+#        t.left(90)
+#    t.home
+
+#def drawcircle(radius, extent = 90, color = 'Blue'):
+#    t.penup()
+#    t.goto(radius, 0)
+#    t.pendown
+#    t.left(90)
+#    t.pencolor(color)
+#    t.circle(radius, extent)
+
+#x = 0
+#y = 0
+#number = 0
+#def getrandxy():
+#    global x
+#    global y
+#    x = random.randint(0, 200)
+#    y = random.randint(0, 200)
+
+#def getsqrt(x, y):
+#    return math.sqrt(x ** 2 + y ** 2)
+
+#drawsquare(200)
+#drawcircle(200)
+
+#for _ in range(0, 500):
+#    getrandxy()
+#    if getsqrt(x, y) <= 200:
+#        number = number + 1
+#        t.penup
+#        t.goto(x, y)
+#        t.pendown
+#        t.dot(3, 'Red')
+
+#print(number)
+
+#print(1+2, 1-2, 2*3, 5/3, 10%3)
+
+#n1 = float(input())
+#n2 = float(input())
+#print(n1 + n2, n1 - n2, n1 * n2, n1 / n2)
+
